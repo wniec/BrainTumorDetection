7ea1081d4f213e9c75c636dbb9fbe45c2372166b
Jer <jeremiej19@wp.pl>
2024-12-14 22:14:30 +0100

    implement delete

--- braintumorsegmentation/db_utils.py
+++ braintumorsegmentation/db_utils.py
@@ -1,14 +1,17 @@
 import sqlite3
+from models import InternalPatient
+# import atexit
 
 class db_conn():
     def __init__(self):
         self.con = sqlite3.connect("patients.db")
+        # atexit.register(self.close)
         self.cur = self.con.cursor()
         # res = cur.execute("SELECT name FROM sqlite_master")
         # name = res.fetchall()
         self.cur.execute("""
         CREATE TABLE IF NOT EXISTS Patient (
-            patient_id INTEGER PRIMARY KEY ,
+            patient_id TEXT PRIMARY KEY,
             last_name varchar(255) NOT NULL,
             first_name varchar(255),
             birth_date DATE
@@ -24,6 +27,7 @@ class db_conn():
             FOREIGN KEY (Patient_id) REFERENCES Patient(Patient_id)
         );
                     """)
+        
 
     def __enter__(self):
         return self
@@ -34,15 +38,31 @@ class db_conn():
     def close(self):
         self.con.close()
         
-    def add_pacient(self, lastName, firstName=None, birth=None):
+    def clear_all_data(self):
+        self.cur.execute("""
+              DELETE FROM Image
+              """)
+        self.cur.execute("""
+              DELETE FROM Patient
+              """)
+        self.con.commit()
+        
+    def uncomplete_all(self):
+        res = self.cur.execute("""
+              UPDATE Image 
+              SET completed = 0
+                               """)
+        self.con.commit()
+        
+    def add_pacient(self, patient_id, lastName,  firstName=None, birth=None):
         res = self.cur.execute("""
               INSERT INTO Patient (patient_id, last_name, first_name, birth_date)
               VALUES(?,?,?,?)
-                               """, (None, lastName, firstName, birth))
+                               """, (patient_id, lastName, firstName, birth))
         self.con.commit()
         return self.cur.rowcount
         
-    def add_imaging(self,pacient_id, danger, scan_date=None):
+    def add_imaging(self, pacient_id, danger, scan_date=None):
         res = self.cur.execute("""
               INSERT INTO Image (image_id, patient_id, scan_date, danger)
               VALUES(?,?,?,?)
@@ -57,26 +77,25 @@ class db_conn():
               SET completed = ?
               WHERE Patient_id = ?
                                """, (status, pacient_id))
+        self.con.commit()
         
     def get_top_not_completed_pacients_ordered(self):
         res = self.cur.execute("""
               SELECT Patient.patient_id, Patient.last_name, Patient.first_name, Image.danger
               FROM Image INNER JOIN Patient ON Patient.Patient_id = Image.Patient_id
-              WHERE Image.Completed = FALSE
+              WHERE Image.Completed = 0
               ORDER BY Image.danger DESC
               LIMIT 5
                                """)
+        data = res.fetchall()
+        return [ InternalPatient(
+            id=str(pacient[0]),
+            name=pacient[1],
+            danger=pacient[3]
+        ) for pacient in data  ]
+    
+    def get_all(self):
+        res = self.cur.execute("""
+              SELECT * FROM Image
+                               """)
         return res.fetchall()
-    
-    # def get_all(self):
-    #     res = self.cur.execute("""
-    #           SELECT * FROM Image
-    #                            """)
-    #     return res.fetchall()
-    
-    # def get_cmp(self):
-    #     res = self.cur.execute("""
-    #           SELECT completed FROM Image
-    #                            """)
-    #     return res.fetchall()
-    
